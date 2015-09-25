bec056cd23b815c5134c55a13b8d9eae0355a30f
Josh <jarhoads@gmail.com>
2015-09-25 14:45:32 -0400

    Updates for AppBar navigation
    Added the AppBar with Home button to the top of the page.

--- RestaurantManager/RestaurantManager/ExpeditePage.xaml.cs
+++ RestaurantManager/RestaurantManager/ExpeditePage.xaml.cs
@@ -26,5 +26,10 @@ namespace RestaurantManager
         {
             this.InitializeComponent();
         }
+
+        private void ExpediteAppButtonHome_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
     }
 }
