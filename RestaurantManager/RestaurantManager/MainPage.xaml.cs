cf9ac92a1563ab028968371acc7cd076ab20becd
Josh <jarhoads@gmail.com>
2015-09-25 10:59:19 -0400

    Adding Navigation for MainPage
    Adding OrdersPage navigation

--- RestaurantManager/RestaurantManager/MainPage.xaml.cs
+++ RestaurantManager/RestaurantManager/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace RestaurantManager
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(OrderPage));
+        }
     }
 }
